296e86178cdcd56c4c8879ee39525c76a885f5e0
Ovidiu Cherecheș <skidding@gmail.com>
2016-12-20 16:13:18 +0200

    Simplify tracing, show changed vars & return value

--- binary-search.js
+++ binary-search.js
@@ -25,43 +25,35 @@ function binarySearch(list, item) {
 
   let low = 0;
   trace({
-    type: 'assign',
     line: 1,
     context: { list, item, low },
   });
 
   let high = list.length - 1;
   trace({
-    type: 'assign',
     line: 2,
     context: { list, item, low, high },
   });
 
   while (trace({
-    type: 'compare',
     line: 4,
     context: { list, item, low, high },
-    compared: ['low', 'high'],
   }) && low <= high) {
     const mid = Math.round((low + high) / 2);
     trace({
-      type: 'assign',
       line: 5,
       context: { list, item, low, high, mid },
     });
 
     const guess = list[mid];
     trace({
-      type: 'assign',
       line: 6,
       context: { list, item, low, high, mid, guess },
     });
 
     trace({
-      type: 'compare',
       line: 8,
       context: { list, item, low, high, mid, guess },
-      compared: ['guess', 'item'],
     });
     if (guess === item) {
       trace({
@@ -74,22 +66,18 @@ function binarySearch(list, item) {
     }
 
     trace({
-      type: 'compare',
       line: 11,
       context: { list, item, low, high, mid, guess },
-      compared: ['guess', 'item'],
     });
     if (guess > item) {
       high = mid - 1;
       trace({
-        type: 'assign',
         line: 12,
         context: { list, item, low, high, mid, guess },
       });
     } else {
       low = mid + 1;
       trace({
-        type: 'assign',
         line: 14,
         context: { list, item, low, high, mid, guess },
       });
@@ -109,7 +97,7 @@ const items = [
   'bear', 'cat', 'cow', 'dog', 'fox', 'pig', 'rat',
 ];
 
-binarySearch(items, 'bear');
+binarySearch(items, 'fox');
 
 const React = require('react');
 const ReactDOM = require('react-dom');
@@ -163,12 +151,17 @@ SourceCode.propTypes = {
 
 function Preview({
   context,
+  changedKeys,
 }) {
   return (
     <pre style={{ backgroundColor: 'lightgray' }}>
       {Object.keys(context).map(key =>
         <div key={key}>
-          {key} = {JSON.stringify(context[key])}
+          <span
+            style={{ fontWeight: changedKeys.indexOf(key) === -1 ? 'normal' : 'bold'}}
+          >
+            {key} = {JSON.stringify(context[key])}
+          </span>
         </div>
       )}
     </pre>
@@ -177,18 +170,21 @@ function Preview({
 
 Preview.propTypes = {
   context: React.PropTypes.object.isRequired,
+  changedKeys: React.PropTypes.array.isRequired,
 };
 
 class App extends React.Component {
   render() {
-    const { steps, step, onPrev, onNext } = this.props;
-    const { line, context } = steps[step];
+    const { steps, stepIndex, onPrev, onNext } = this.props;
+    const { type, line, context, returnValue } = steps[stepIndex];
+    const prevStep = steps[stepIndex - 1];
+    const prevContext = prevStep ? prevStep.context : {};
 
     return (
       <div>
         <div>
-          <button disabled={step <= 0} onClick={onPrev}>back</button>
-          <button disabled={step >= steps.length - 1} onClick={onNext}>forward</button>
+          <button disabled={stepIndex <= 0} onClick={onPrev}>back</button>
+          <button disabled={stepIndex >= steps.length - 1} onClick={onNext}>forward</button>
         </div>
         <SourceCode
           def={fnDef}
@@ -196,7 +192,14 @@ class App extends React.Component {
         />
         <Preview
           context={context}
+          changedKeys={Object.keys(context).filter(key => context[key] !== prevContext[key])}
+          returnValue={returnValue}
         />
+        {type === 'return' && (
+          <pre style={{ backgroundColor: 'gray', color: 'white' }}>
+            Return: {JSON.stringify(returnValue)}
+          </pre>
+        )}
       </div>
     );
   }
@@ -204,7 +207,7 @@ class App extends React.Component {
 
 App.propTypes = {
   steps: React.PropTypes.array.isRequired,
-  step: React.PropTypes.number.isRequired,
+  stepIndex: React.PropTypes.number.isRequired,
   onPrev: React.PropTypes.func.isRequired,
   onNext: React.PropTypes.func.isRequired,
 };
@@ -213,11 +216,11 @@ App.propTypes = {
 const container = document.body.appendChild(document.createElement('div'));
 let currentStep = 0;
 
-function render(step) {
+function render(stepIndex) {
   ReactDOM.render((
     <App
       steps={steps}
-      step={step}
+      stepIndex={stepIndex}
       onPrev={() => {
         render(currentStep -= 1);
       }}
