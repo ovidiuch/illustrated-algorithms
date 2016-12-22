d9ddfa6bb40600c6e78399659dad6728c9de4835
Ovidiu Cherecheș <skidding@gmail.com>
2016-12-22 16:28:30 +0200

    Bring back line highlighting & numbering

--- components/source-code.js
+++ components/source-code.js
@@ -5,17 +5,36 @@ export default function SourceCode({
   start,
   end,
 }) {
+  let lineStart = 0;
   return (
     <pre>
-      <span>
-        {def.slice(0, start)}
-      </span>
-      <span style={{ backgroundColor: 'yellow' }}>
-        {def.slice(start, end)}
-      </span>
-      <span>
-        {def.slice(end)}
-      </span>
+      <ol>
+        {def.split('\n').map((fnLine, i) => {
+          const lineLen = fnLine.length;
+          const lineEnd = lineStart + lineLen;
+          const isRangeInLine = start < lineEnd && end > lineStart;
+          const relStart = start - lineStart;
+          const relEnd = end - lineStart;
+          lineStart += lineLen + 1; // account for newlines removed
+
+          return (
+            <li
+              key={i}
+              style={{ backgroundColor: isRangeInLine ? 'rgba(255, 255, 0, 0.4)' : 'transparent' }}
+            >
+              {isRangeInLine ? (
+                <span>
+                  <span>{fnLine.slice(0, relStart)}</span>
+                  <span style={{ backgroundColor: 'lightblue' }}>
+                    {fnLine.slice(relStart, relEnd)}
+                  </span>
+                  <span>{fnLine.slice(relEnd)}</span>
+                </span>
+              ) : fnLine}
+            </li>
+          );
+        })}
+      </ol>
     </pre>
   );
 }
