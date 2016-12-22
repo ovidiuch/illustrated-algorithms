b9a6a3409d7ca37515bada13b55bb3e26642c8ae
Ovidiu Cherecheș <skidding@gmail.com>
2016-12-22 16:33:49 +0200

    Select random pivot in quick sort

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -1,9 +1,13 @@
+function random(list) {
+  return list[Math.round(Math.random() * (list.length - 1))];
+}
+
 export default function quicksort(list) {
   if (list.length < 2) {
     return list;
   }
 
-  const pivot = list[0]; // TODO: random
+  const pivot = random(list);
   const less = list.filter(i => i < pivot);
   const greater = list.filter(i => i > pivot);
 
