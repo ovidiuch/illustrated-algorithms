e5251c07ca2b139823d56354b359d5097a0c31ee
Ovidiu Cherecheș <skidding@gmail.com>
2017-01-10 17:01:46 +0200

    Make sure wobble deg stops at 0

--- utils/wobble.js
+++ utils/wobble.js
@@ -1,7 +1,9 @@
 const TIMES = 2;
 const MAX_ANGLE = 5;
 
-const getWobbleRotation = stepProgress =>
-  Math.sin(Math.PI * 2 * TIMES * stepProgress) * MAX_ANGLE;
+const getWobbleRotation = stepProgress => {
+  const rotation = Math.sin(Math.PI * 2 * TIMES * stepProgress) * MAX_ANGLE;
+  return Math.round(rotation * 100) / 100;
+};
 
 export default getWobbleRotation;
