20e4b956dd8f16af91896e29b10847253e39c9ac
Ovidiu Cherecheș <skidding@gmail.com>
2017-01-14 14:20:03 +0200

    Optimize RawData illustration

--- components/illustrations/raw-data.js
+++ components/illustrations/raw-data.js
@@ -1,79 +1,88 @@
 import React from 'react';
 import RawDataLayout from '../../layout/raw-data';
 
-export default function RawData({
-  nextStep,
-}, {
-  layout
-}) {
-  const {
-    bindings,
-    returnValue,
-  } = nextStep;
-  const {
-    padding,
-    fontSize,
-    lineHeight,
-    illustrationHeight,
-  } = layout;
+class RawData extends React.Component {
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    // This component doesn't do any animation and only prints the next step
+    return nextProps.nextStep !== this.props.nextStep || nextContext.layout !== this.context.layout;
+  }
 
-  return (
-    <div
-      className="raw-data"
-      style={{
-        height: illustrationHeight,
-        padding,
-        fontSize,
-        lineHeight: `${lineHeight}px`,
-      }}
-      >
+  render() {
+    const {
+      nextStep,
+    } = this.props;
+    const {
+      layout
+    } = this.context;
+    const {
+      bindings,
+      returnValue,
+    } = nextStep;
+    const {
+      padding,
+      fontSize,
+      lineHeight,
+      illustrationHeight,
+    } = layout;
+
+    return (
       <div
-        className="inner"
+        className="raw-data"
         style={{
-          left: padding,
-          right: padding,
+          height: illustrationHeight,
+          padding,
+          fontSize,
+          lineHeight: `${lineHeight}px`,
         }}
         >
-        {Object.keys(bindings).map(key =>
-          <pre
-            key={key}
-            style={{ padding }}
-            >
-            {key} = {JSON.stringify(bindings[key])}
-          </pre>
-        )}
-        {returnValue !== undefined && (
-          <pre
-            className="return-value"
-            style={{ padding }}
-            >
-            {JSON.stringify(returnValue)}
-          </pre>
-        )}
+        <div
+          className="inner"
+          style={{
+            left: padding,
+            right: padding,
+          }}
+          >
+          {Object.keys(bindings).map(key =>
+            <pre
+              key={key}
+              style={{ padding }}
+              >
+              {key} = {JSON.stringify(bindings[key])}
+            </pre>
+          )}
+          {returnValue !== undefined && (
+            <pre
+              className="return-value"
+              style={{ padding }}
+              >
+              {JSON.stringify(returnValue)}
+            </pre>
+          )}
+        </div>
+        <style jsx>{`
+          .raw-data {
+            position: relative;
+            box-sizing: border-box;
+          }
+          .inner {
+            position: absolute;
+            top: 50%;
+            transform: translate(0, -50%);
+          }
+          pre {
+            margin: 0;
+            background: rgba(0, 0, 0, 0.5);
+            color: white;
+            font-family: 'FiraCode-Light', monospace;
+            white-space: pre-wrap;
+          }
+          .return-value {
+            background: rgba(0, 0, 0, 0.8);
+          }
+        `}</style>
       </div>
-      <style jsx>{`
-        .raw-data {
-          position: relative;
-          box-sizing: border-box;
-        }
-        .inner {
-          position: absolute;
-          top: 50%;
-          transform: translate(0, -50%);
-        }
-        pre {
-          margin: 0;
-          background: rgba(0, 0, 0, 0.5);
-          color: white;
-          font-family: 'FiraCode-Light', monospace;
-          white-space: pre-wrap;
-        }
-        .return-value {
-          background: rgba(0, 0, 0, 0.8);
-        }
-      `}</style>
-    </div>
-  );
+    );
+  }
 }
 
 RawData.propTypes = {
@@ -85,3 +94,5 @@ RawData.contextTypes = {
 };
 
 RawData.Layout = RawDataLayout;
+
+export default RawData;
