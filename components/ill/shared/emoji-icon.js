a0f190cc7ce7f15f8bee170d3439274c6a350c1d
Ovidiu Cherecheș <skidding@gmail.com>
2017-02-06 11:03:03 +0200

    Add new emoji for Quicksort

--- components/ill/shared/emoji-icon.js
+++ components/ill/shared/emoji-icon.js
@@ -1,24 +1,31 @@
 import Bear from 'emojione/assets/svg/1f43b.svg';
 import Cat from 'emojione/assets/svg/1f431.svg';
 import Dog from 'emojione/assets/svg/1f436.svg';
-// import Gorilla from 'emojione/assets/svg/1f98d.svg';
 import Lion from 'emojione/assets/svg/1f981.svg';
 import Panda from 'emojione/assets/svg/1f43c.svg';
-// import Pig from 'emojione/assets/svg/1f437.svg';
-// import Rat from 'emojione/assets/svg/1f400.svg';
 import Snail from 'emojione/assets/svg/1f40c.svg';
+import Cherries from 'emojione/assets/svg/1f352.svg';
+import Kiwi from 'emojione/assets/svg/1f95d.svg';
+import Grapes from 'emojione/assets/svg/1f347.svg';
+import Avocado from 'emojione/assets/svg/1f951.svg';
+import Peach from 'emojione/assets/svg/1f351.svg';
+import Pineapple from 'emojione/assets/svg/1f34d.svg';
 import React from 'react';
 
 const emojis = {
   bear: Bear,
   cat: Cat,
   dog: Dog,
-  // gorilla: Gorilla,
   lion: Lion,
   panda: Panda,
-  // pig: Pig,
-  // rat: Rat,
   snail: Snail,
+
+  cherries: Cherries,
+  kiwi: Kiwi,
+  grapes: Grapes,
+  avocado: Avocado,
+  peach: Peach,
+  pineapple: Pineapple,
 };
 
 export default class EmojiIcon extends React.PureComponent {
