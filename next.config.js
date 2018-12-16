4d71a66b36e4b48fb09fa64161e995a10cfc9a42
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-16 19:33:23 +0200

    Publish export instead of running instance (#30)

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+      '/binary-search': { page: '/binary-search' },
+      '/quicksort': { page: '/quicksort' },
+      '/bfs': { page: '/bfs' }
+    };
+  }
+};
